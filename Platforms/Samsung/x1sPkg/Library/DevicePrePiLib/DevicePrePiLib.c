7cca43ce185d7ed386256de1f5fb5a367da261ab
halal-beef <78730004+halal-beef@users.noreply.github.com>
2024-07-20 13:08:03 +0100

    Revert PrePi changes

--- Platforms/Samsung/x1sPkg/Library/DevicePrePiLib/DevicePrePiLib.c
+++ Platforms/Samsung/x1sPkg/Library/DevicePrePiLib/DevicePrePiLib.c
@@ -1,48 +1,4 @@
-#include <Library/BaseLib.h>
 #include <Library/DevicePrePiLib.h>
-#include <Library/DebugLib.h>
-#include <Library/IoLib.h>
-
-#include "libfdt.h"
 
 VOID
-DeviceInitialize ()
-{
-  //UINT32 fdtBase = MmioRead32(0x92200000);
-
-  //DEBUG((EFI_D_WARN, "\n\n\n\nFDT Base: 0x%x\n", fdtBase));
-
-  //UINT32 fdt = MmioRead32(0x8A080000);
-  //DEBUG((EFI_D_WARN, "FDT MAGIC: 0x%x\n", fdt));
-
-  CONST VOID *fdtP = (CONST VOID *)(0x8A080000);
-
-  //if (fdt_check_header(fdtP) == 0) DEBUG((EFI_D_WARN, "ITS AN FDT JIM\n"));
-
-  INT32 offset = 0;
-
-      while ((offset = fdt_next_node(fdtP, offset, NULL)) >= 0) {
-CONST CHAR8 *node_name = fdt_get_name(fdtP, offset, NULL);
-
-        if (node_name && AsciiStrStr(node_name, "decon_f@")) {
-
-	    //DEBUG((EFI_D_WARN, "Node: %a\n", node_name));
-            int len;
-            const fdt32_t *reg = fdt_getprop(fdtP, offset, "reg", &len);
-            if (reg && len > 0) {
-                int num_regs = len / sizeof(fdt32_t);
-                for (int i = 0; i < num_regs; i++) {
-		    UINT32 reg_addr = fdt32_to_cpu(reg[i]);
-			if(reg_addr != 0x0 && reg_addr != 0x10000)
-			{
-			  MmioWrite32(reg_addr + 0x70, 0x1281);
-			  DEBUG((EFI_D_WARN, "\n\n\nIf you see this, decon has been enabled via FDT Filtering\n\n"));
-			}
-                }
-            }
-}
-}
-//  if(FdtCheckHeader (fdt) == 0) DEBUG((EFI_D_WARN, "ITS AN FDT JIM\n"));
-
-//  while(TRUE){}
-}
+DeviceInitialize () {}
--- Silicon/Samsung/E9830Pkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
+++ Silicon/Samsung/E9830Pkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
@@ -8,7 +8,7 @@ VOID
 PlatformInitialize ()
 {
   // Enable Framebuffer
-  //MmioWrite32 (DECON_F_BASE + HW_SW_TRIG_CONTROL, 0x1281);
+  MmioWrite32 (DECON_F_BASE + HW_SW_TRIG_CONTROL, 0x1281);
 
   // Run Device Specific Code
   DeviceInitialize ();
