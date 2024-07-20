f114a1a8e62009a190fae33ec95d5be518206505
halal-beef <78730004+halal-beef@users.noreply.github.com>
2024-07-20 11:03:20 +0100

    Decon stuff, still very dirty

--- Platforms/Samsung/x1sPkg/Library/DevicePrePiLib/DevicePrePiLib.c
+++ Platforms/Samsung/x1sPkg/Library/DevicePrePiLib/DevicePrePiLib.c
@@ -8,37 +8,41 @@
 VOID
 DeviceInitialize ()
 {
-  UINT32 fdtBase = MmioRead32(0x92200000);
+  //UINT32 fdtBase = MmioRead32(0x92200000);
 
-  DEBUG((EFI_D_WARN, "\n\n\n\nFDT Base: 0x%x\n", fdtBase));
-DEBUG((EFI_D_ERROR, "\n\n\n\nFDT Base: 0x%x\n", fdtBase));
+  //DEBUG((EFI_D_WARN, "\n\n\n\nFDT Base: 0x%x\n", fdtBase));
 
-  UINT32 fdt = MmioRead32(0x8A080000);
-  DEBUG((EFI_D_WARN, "FDT MAGIC: 0x%x\n", fdt));
+  //UINT32 fdt = MmioRead32(0x8A080000);
+  //DEBUG((EFI_D_WARN, "FDT MAGIC: 0x%x\n", fdt));
 
   CONST VOID *fdtP = (CONST VOID *)(0x8A080000);
 
-  if (fdt_check_header(fdtP) == 0) DEBUG((EFI_D_WARN, "ITS AN FDT JIM\n"));
+  //if (fdt_check_header(fdtP) == 0) DEBUG((EFI_D_WARN, "ITS AN FDT JIM\n"));
 
   INT32 offset = 0;
 
       while ((offset = fdt_next_node(fdtP, offset, NULL)) >= 0) {
-const char *node_name = fdt_get_name(fdtP, offset, NULL);
+CONST CHAR8 *node_name = fdt_get_name(fdtP, offset, NULL);
 
-        if (node_name && AsciiStrStr(node_name, "decon_f")) {
+        if (node_name && AsciiStrStr(node_name, "decon_f@")) {
 
-	    DEBUG((EFI_D_WARN, "Node: %s\n", node_name));
+	    //DEBUG((EFI_D_WARN, "Node: %a\n", node_name));
             int len;
             const fdt32_t *reg = fdt_getprop(fdtP, offset, "reg", &len);
             if (reg && len > 0) {
                 int num_regs = len / sizeof(fdt32_t);
                 for (int i = 0; i < num_regs; i++) {
-                    DEBUG((EFI_D_ERROR, "0x%08x\n", fdt32_to_cpu(reg[i])));
+		    UINT32 reg_addr = fdt32_to_cpu(reg[i]);
+			if(reg_addr != 0x0 && reg_addr != 0x10000)
+			{
+			  MmioWrite32(reg_addr + 0x70, 0x1281);
+			  DEBUG((EFI_D_WARN, "\n\n\nIf you see this, decon has been enabled via FDT Filtering\n\n"));
+			}
                 }
             }
 }
 }
 //  if(FdtCheckHeader (fdt) == 0) DEBUG((EFI_D_WARN, "ITS AN FDT JIM\n"));
 
-  while(TRUE){}
+//  while(TRUE){}
 }
